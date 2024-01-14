75a96a6abd9c75f7e965fe00c2ece32ea497fbda
NguyenVinh <57793702+vinhvrs@users.noreply.github.com>
2024-01-14 13:20:56 +0700

    Add files via upload
    update user profile

--- userprofile.aspx.designer.cs
+++ userprofile.aspx.designer.cs
@@ -19,7 +19,7 @@ namespace OnlineBookstore
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
-        /// To modify move field declaration FROM designer file to code-behind file.
+        /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label Label1;
 
@@ -28,7 +28,7 @@ namespace OnlineBookstore
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
-        /// To modify move field declaration FROM designer file to code-behind file.
+        /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox TextBox1;
 
@@ -37,7 +37,7 @@ namespace OnlineBookstore
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
-        /// To modify move field declaration FROM designer file to code-behind file.
+        /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox TextBox2;
 
@@ -46,7 +46,7 @@ namespace OnlineBookstore
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
-        /// To modify move field declaration FROM designer file to code-behind file.
+        /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox TextBox3;
 
@@ -55,7 +55,7 @@ namespace OnlineBookstore
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
-        /// To modify move field declaration FROM designer file to code-behind file.
+        /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox TextBox4;
 
@@ -64,7 +64,7 @@ namespace OnlineBookstore
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
-        /// To modify move field declaration FROM designer file to code-behind file.
+        /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
 
@@ -73,7 +73,7 @@ namespace OnlineBookstore
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
-        /// To modify move field declaration FROM designer file to code-behind file.
+        /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox TextBox6;
 
@@ -82,7 +82,7 @@ namespace OnlineBookstore
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
-        /// To modify move field declaration FROM designer file to code-behind file.
+        /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox TextBox7;
 
@@ -91,7 +91,7 @@ namespace OnlineBookstore
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
-        /// To modify move field declaration FROM designer file to code-behind file.
+        /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox TextBox5;
 
@@ -100,7 +100,7 @@ namespace OnlineBookstore
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
-        /// To modify move field declaration FROM designer file to code-behind file.
+        /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox TextBox8;
 
@@ -109,7 +109,7 @@ namespace OnlineBookstore
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
-        /// To modify move field declaration FROM designer file to code-behind file.
+        /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox TextBox9;
 
@@ -118,7 +118,7 @@ namespace OnlineBookstore
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
-        /// To modify move field declaration FROM designer file to code-behind file.
+        /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox TextBox10;
 
@@ -127,7 +127,7 @@ namespace OnlineBookstore
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
-        /// To modify move field declaration FROM designer file to code-behind file.
+        /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button Button1;
 
@@ -136,7 +136,7 @@ namespace OnlineBookstore
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
-        /// To modify move field declaration FROM designer file to code-behind file.
+        /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label Label2;
 
@@ -145,7 +145,7 @@ namespace OnlineBookstore
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
-        /// To modify move field declaration FROM designer file to code-behind file.
+        /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView GridView1;
     }
